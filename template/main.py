517025298f46ab56900aaf63c736f49c3c4626b2
David Wessels <davidwessels15@gmail.com>
2023-01-05 20:22:58 +0100

    template pytest

---
+++ template/main.py
@@ -0,0 +1,9 @@
+from template.add import addition
+from template.minus import minus
+
+def main():
+    print(addition(1, 2))
+    print(minus(1, 2))
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_add.py
@@ -0,0 +1,5 @@
+from template.add import addition
+
+def test_add():
+    assert addition(1, 2) == 3
+    assert addition(2, 2) == 4
